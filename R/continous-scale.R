3ddb9a3d31ae97558eeebcba98ccb8bea79b3c3a
Nishtha <naro0004@student.monash.edu>
2021-10-22 09:12:00 +0530

    Checking .r files with examples

--- R/continous-scale.R
+++ R/continous-scale.R
@@ -2,10 +2,13 @@
 
 scale_colour_warmCrush_seq_c <- function(name = "Rust", direction = 1, amount = 0.2, ...) {
 
-  if (!name %in% names(warmCrush_colours)){
-    stop("Color not in palette")
-  }
+  if (!name %in% names(warmCrush_colours))
+  {
 
+    stop(paste0("Color not in palette", names(warmCrush_colours)))
+
+
+  }
   low = warmCrush_colours[[name]]
 
   high = colorspace::lighten(warmCrush_colours[[name]], amount = amount, space = "combined", fixup = TRUE)
